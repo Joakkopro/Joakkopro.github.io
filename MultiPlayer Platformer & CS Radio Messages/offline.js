df6986d328c0f6d59f5435dc365ce68286bbddef
Joakkopro <anita.lealg3@gmail.com>
2025-06-14 10:55:38 -0400

    nuevo juego

---
+++ MultiPlayer Platformer & CS Radio Messages/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version"; 1749912433,
+	"fileList"; [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/me-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"media/audio1.ogg",
+		"media/audio2.ogg",
+		"media/audio3.ogg",
+		"media/audio4.ogg",
+		"media/audio5.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
